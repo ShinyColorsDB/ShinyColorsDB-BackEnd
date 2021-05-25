8dbf577705d90e1e81e93f1b0ae78d3535679192
euphokumiko <h.k.h94539@gmail.com>
2021-05-25 15:30:25 +0800

    update

---
+++ app.js
@@ -0,0 +1,43 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let cors = require("cors");
+
+let spineRouter = require('./routes/spine');
+//let picRouter = require('./routes/pic');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.route("/spine", spineRouter);
+app.route("/pic", picRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = app;
---
+++ routes/spine.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const spine = express.Router();
+const mysql2 = require('mysql2');
+const config = require('../config.json');
+const conn = mysql2.createConnection(config);
+
+spine.get("/idolList.json", (req, res, next) => {
+    let data = new Array();
+    conn.query("SELECT `IdolID`, `NickName`, `IdolName` FROM `1-Idols`", (err, result) => {
+        result.forEach(element => {
+            data.push({ IdolID: element.IdolID, Directory: element.NickName, IdolName: element.IdolName });
+        });
+        res.send(data);
+    });
+});
+
+module.exports = spine;
