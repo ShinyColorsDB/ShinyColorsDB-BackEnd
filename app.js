36d34804227bb9e13067a8ca40e6098a9dff89e3
euphokumiko <h.k.h94539@gmail.com>
2021-06-13 02:33:15 +0800

    test

--- app.js
+++ app.js
@@ -3,40 +3,32 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-let cors = require("cors");
 
-let spineRouter = require('./routes/spine.js');
-//let picRouter = require('./routes/pic');
+let spineRouter = require('./routes/spines.js');
 
 let app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/spines", spineRouter);
-//app.route("/pic", picRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/spines.js
+++ routes/spines.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const spine = express.Router();
+const spines = express.Router();
 const mysql2 = require('mysql2');
 const config = require('../config.json');
 const conn = mysql2.createConnection(config);
 
-spine.get("/idolList", (req, res, next) => {
+spines.get("/idolList", (req, res, next) => {
     let data = new Array();
     conn.query("SELECT `IdolID`, `NickName`, `IdolName` FROM `1-Idols`", (err, result) => {
         result.forEach(element => {
@@ -14,19 +14,10 @@ spine.get("/idolList", (req, res, next) => {
     });
 });
 
-spine.get("/dressList/:IdolID", (req, res, next) => {
-    let data = new Array();
+spines.get("/dressList/:IdolID", (req, res, next) => {
     conn.query("SELECT * FROM `20-IdolDresses` WHERE `IdolID` = ? ORDER BY FIELD (`DressType`, \"P_SSR\", \"P_SR\", \"Anniversary\", \"Special\"), `DressIndex`", [req.params.IdolID], (err, result) => {
         res.send(result);
     });
 });
 
-spine.get("typeList/:DressIndex", (req, res, next) => {
-
-})
-
-spine.get("/", (req, res, next) => {
-    res.send("test");
-});
-
-module.exports = spine;
+module.exports = spines;
