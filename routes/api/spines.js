c53bce29b1aba18c6c4d5cc2369dcfd5868b4e07
euphokumiko <h.k.h94539@gmail.com>
2021-07-25 19:44:39 +0800

    Update spines.js

--- routes/api/spines.js
+++ routes/api/spines.js
@@ -15,7 +15,7 @@ spines.get("/idolList", (req, res, next) => {
 });
 
 spines.get("/dressList/:IdolID", (req, res, next) => {
-    conn.query("SELECT * FROM `20-IdolDresses` WHERE `IdolID` = ? ORDER BY FIELD (`DressType`, \"P_SSR\", \"P_SR\", \"Anniversary\", \"Special\"), `DressIndex`", [req.params.IdolID], (err, result) => {
+    conn.query("SELECT * FROM `20-IdolDresses` WHERE `IdolID` = ? ORDER BY FIELD (`DressType`, \"P_SSR\", \"P_SR\", \"Anniversary\", \"Mizugi\",\"Special\"), `DressOrder`", [req.params.IdolID], (err, result) => {
         res.send(result);
     });
 });
