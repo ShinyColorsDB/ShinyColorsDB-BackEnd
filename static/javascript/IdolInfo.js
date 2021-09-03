8aa1f5f817f1a01a76558de10f63d2be0d5a23d6
euphokumiko <h.k.h94539@gmail.com>
2021-09-03 23:07:04 +0800

    update js

--- static/javascript/IdolInfo.js
+++ static/javascript/IdolInfo.js
@@ -1,70 +1,29 @@
-$(function() {
-    let count = 0;
-    $(".cardElement").click(function(e) {
-        let pic = $(this).attr("data-picture"), link = $(this).attr("data-carduuid"), name = $(this).attr("data-cardname");
-        $("#cardPicture").attr("src", `https://static.shinycolors.moe/pictures/bigPic/${pic}`);
-        if ($("#PList").hasClass("active")) {
-            $('#cardInfoLink').attr("href", `/info/PCardInfo?UUID=${link}`).text(name);
-            $('#cardInfoLinkImg').attr("href", `/info/PCardInfo?UUID=${link}`);
-        }
-        else {
-            $('#cardInfoLink').attr("href", `/info/SCardInfo?UUID=${link}`).text(name);
-            $('#cardInfoLinkImg').attr("href", `/info/SCardInfo?UUID=${link}`);
-        }
+let currentPCard = true;
+
+function Init() {
+    let pList = document.getElementById("PList"),
+        sList = document.getElementById("SList");
+    let pCard = document.getElementById("divCardProduce"),
+        sCard = document.getElementById("divCardSupport");
+    pList.addEventListener("click", (e) => {
+        e.preventDefault();
+        if(currentPCard) return;
+
+        sList.classList.remove("active", "disabled");
+        pList.classList.add("active", "disabled");
+        currentPCard = true;
+        pCard.classList.remove("d-none");
+        sCard.classList.add("d-none");
     });
 
-    $(".cardType").click(function(e) {
-        if($(this).hasClass("active") || $(this).hasClass("disabled")) return;
+    sList.addEventListener("click", (e) => {
+        e.preventDefault();
+        if(!currentPCard) return;
 
-        $(".cardType").removeClass("active disabled");
-        $(this).addClass("active disabled");
-
-        $(".cardList").addClass("d-none");
-        $(".cardRarity").removeClass("active disabled");
-        $("#SSRList").addClass("active disabled");
-        $("#cardTypeTitle").text(`${$(this).attr("data-cardType")}-SSR`);
-        
-        if($(this).attr("data-cardType") == "P"){
-            $("#NList").addClass("disabled");
-            $("#PSSRCardList").removeClass("d-none");
-        }
-        else if($(this).attr("data-cardType") == "S") {
-            $("#NList").removeClass("disabled");
-            $("#SSSRCardList").removeClass("d-none");
-        }
+        pList.classList.remove("active", "disabled");
+        sList.classList.add("active", "disabled");
+        currentPCard = false;
+        sCard.classList.remove("d-none");
+        pCard.classList.add("d-none");
     });
-
-    $(".cardRarity").click(function() {
-        if($(this).hasClass("active") || $(this).hasClass("disabled")) return;
-
-        $(".cardRarity").removeClass("active disabled");
-        $(this).addClass("active disabled");
-
-        if($(".cardType.active.disabled").attr("data-cardType") == "P") {
-            $("#NList").addClass("disabled");
-        }
-
-        let type = $(".active.cardType").attr("data-cardType");
-        let rari = $(".active.cardRarity").attr("data-cardRarity");
-        $(".cardList").addClass("d-none");
-        $(`#${type}${rari}`).removeClass("d-none");
-        $("#cardTypeTitle").text(`${type}-${$(this).attr("data-title")}`);
-    });
-
-    $("#nextTachie").click(function() {
-        count = count + 1 < 3 ? count + 1 : 0;
-
-        $(".tachie").addClass("d-none");
-        switch(count) {
-            case 0: 
-                $("#imgP").removeClass("d-none");
-                break;
-            case 1:
-                $("#imgS").removeClass("d-none");
-                break;
-            case 2:
-                $("#imgL").removeClass("d-none");
-                break;
-        }
-    });
-});
+}
