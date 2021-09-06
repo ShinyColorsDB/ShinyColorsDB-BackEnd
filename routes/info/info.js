b851c5cc67e8400249b785bc78bb0cc51dd77498
euphokumiko <h.k.h94539@gmail.com>
2021-09-06 12:50:43 +0800

    update

--- routes/info/info.js
+++ routes/info/info.js
@@ -27,7 +27,7 @@ info.get("/PCardInfo", async (req, res, next) => {
     return;
     if (!req.query.UUID) res.redirect("./IdolInfo?IdolID=1");
 
-    const [ListByGroup, List] = await DBGetIdolList();    
+    const [ListByGroup, List] = await DBGetIdolList();
     const [Panel, IdolID] = await DBGetPCardPanel(req.query.UUID);
     const IdolInfo = await DBGetIdolInfo(IdolID);
     const CardInfo = await DBGetPCardInfo(req.query.UUID);
@@ -49,7 +49,7 @@ info.get("/SCardInfo", async (req, res) => {
 
     const [ListByGroup, List] = await DBGetIdolList();
     const [Panel, IdolID] = await DBGetSCardPanel(req.query.UUID);
-    if (!IdolID) { 
+    if (!IdolID) {
         res.sendStatus(404);
         return;
     }
@@ -57,38 +57,38 @@ info.get("/SCardInfo", async (req, res) => {
         e.PanelDesc = e.PanelDesc.replace(/\n/g, "<br>");
     });
     const IdolInfo = await DBGetIdolInfo(IdolID);
-    switch(IdolInfo.Hirameki) {
-        case "Vo": 
+    switch (IdolInfo.Hirameki) {
+        case "Vo":
             IdolInfo.HiraName = "vocal";
             break;
-        case "Da": 
+        case "Da":
             IdolInfo.HiraName = "dance";
             break;
-        case "Vi": 
+        case "Vi":
             IdolInfo.HiraName = "visual"
             break;
-        case "Me": 
+        case "Me":
             IdolInfo.HiraName = "mental"
             break;
     }
     const CardInfo = await DBGetSCardInfo(req.query.UUID);
-    switch(CardInfo.GetMethod) {
-        case "Events": 
+    switch (CardInfo.GetMethod) {
+        case "Events":
             CardInfo.GetMethod = "活動報酬";
             break;
         case "LimitedGasha":
             CardInfo.GetMethod = "限定卡池";
             break;
-        case "GeneralGasha": 
+        case "GeneralGasha":
             CardInfo.GetMethod = "常駐卡池";
             break;
         default:
             break;
     }
-    const SupportSkills = await DBGetSupportSkills(req.query.UUID);
+    const [SupportSkills0, SupportSkills1, SupportSkills2, SupportSkills3, SupportSkills4] = await DBGetSupportSkills(req.query.UUID);
     const SupportEvents = await DBGetSupportEvents(req.query.UUID);
     const IdeaMark = await DBGetIdeaMark(req.query.UUID);
-    switch(IdeaMark.IdeaMark) {
+    switch (IdeaMark.IdeaMark) {
         case "skill_point":
             IdeaMark.IdeaName = "アピール";
             break;
@@ -107,8 +107,8 @@ info.get("/SCardInfo", async (req, res) => {
     }
     const Proficiency = await DBGetProficiency(req.query.UUID);
     Proficiency.forEach(element => {
-        switch(element.Proficiency) {
-            case "sing_ability": 
+        switch (element.Proficiency) {
+            case "sing_ability":
                 element.ProfName = "歌唱力";
                 break;
             case "feel_stable":
@@ -125,13 +125,17 @@ info.get("/SCardInfo", async (req, res) => {
                 break;
         }
     });
-    
+
     res.render('SCardViewer', {
         IdolList: ListByGroup,
         SkillPanel: Panel,
         IdolInfo: IdolInfo,
         CardInfo: CardInfo,
-        SupportSkills: SupportSkills,
+        SupportSkills0: SupportSkills0,
+        SupportSkills1: SupportSkills1,
+        SupportSkills2: SupportSkills2,
+        SupportSkills3: SupportSkills3,
+        SupportSkills4: SupportSkills4,
         SupportEvents: SupportEvents,
         IdeaMark: IdeaMark,
         Proficiency: Proficiency
@@ -223,97 +227,121 @@ function GenerateImgObj(str) {
 
 function DBGetPCardPanel(CardUUID) {
     return new Promise((res, rej) => {
-        conn.execute("SELECT a.CardName, a.IdolID, b.* FROM `3-IdolCards` AS a, `10-CardSkillPanel` AS b WHERE a.CardUUID = ? AND a.CardIndex = b.CardIndex ORDER BY b.SkillSlot", 
-        [CardUUID], 
-        (err, result) => {
-            res([result, result[0].IdolID]);
-        });
+        conn.execute("SELECT a.CardName, a.IdolID, b.* FROM `3-IdolCards` AS a, `10-CardSkillPanel` AS b WHERE a.CardUUID = ? AND a.CardIndex = b.CardIndex ORDER BY b.SkillSlot",
+            [CardUUID],
+            (err, result) => {
+                res([result, result[0].IdolID]);
+            });
     });
 }
 
 function DBGetSCardPanel(CardUUID) {
     return new Promise((res, rej) => {
-        conn.execute("SELECT a.CardName, a.IdolID, b.* FROM `3-IdolCards` AS a, `14-CardSkillPanels` AS b WHERE a.CardUUID = ? AND a.CardIndex = b.CardIndex ORDER BY b.PanelSlot", 
-        [CardUUID], 
-        (err, result) => {
-            if (!result[0]?.IdolID) {
-                res([result, 0]);
-            }
-            else {
-                res([result, result[0].IdolID]);
-            }
-        });
+        conn.execute("SELECT a.CardName, a.IdolID, b.* FROM `3-IdolCards` AS a, `14-CardSkillPanels` AS b WHERE a.CardUUID = ? AND a.CardIndex = b.CardIndex ORDER BY b.PanelSlot",
+            [CardUUID],
+            (err, result) => {
+                if (!result[0]?.IdolID) {
+                    res([result, 0]);
+                }
+                else {
+                    res([result, result[0].IdolID]);
+                }
+            });
     });
 }
 
 function DBGetPCardInfo(CardUUID) {
     return new Promise((res, rej) => {
-        conn.execute("SELECT a.*, b.DressUUID, b.Exist AS `DressExist` FROM `3-IdolCards` AS a, `20-IdolDresses` AS b WHERE a.CardUUID = ? AND a.DressIndex = b.DressIndex;", 
-        [CardUUID], 
-        (err, result) => {
-            res(result[0]);
-        });
+        conn.execute("SELECT a.*, b.DressUUID, b.Exist AS `DressExist` FROM `3-IdolCards` AS a, `20-IdolDresses` AS b WHERE a.CardUUID = ? AND a.DressIndex = b.DressIndex;",
+            [CardUUID],
+            (err, result) => {
+                res(result[0]);
+            });
     });
 }
 
 function DBGetSCardInfo(CardUUID) {
     return new Promise((res, rej) => {
-        conn.execute("SELECT a.* FROM `3-IdolCards` AS a WHERE a.CardUUID = ? ;", 
-        [CardUUID], 
-        (err, result) => {
-            result[0].ReleaseDate = date.format(new Date(result[0].ReleaseDate), 'MM/dd/yyyy');
-            res(result[0]);
-        });
+        conn.execute("SELECT a.* FROM `3-IdolCards` AS a WHERE a.CardUUID = ? ;",
+            [CardUUID],
+            (err, result) => {
+                result[0].ReleaseDate = date.format(new Date(result[0].ReleaseDate), 'MM/dd/yyyy');
+                res(result[0]);
+            });
     });
 }
 
 function DBGetSupportSkills(CardUUID) {
     return new Promise((res, _) => {
-        conn.execute("SELECT a.* FROM `16-CardSupportSkills` AS a, `3-IdolCards` AS b WHERE b.CardUUID = ? AND a.CardIndex = b.CardIndex", 
-        [CardUUID],
-        (err, result) => {
-            let tmp = new Map();
-            result.forEach(element => {
-                if (tmp.has(element.SkillName) && tmp.get(element.SkillName).SkillLevel < element.SkillLevel) {
-                    tmp.set(element.SkillName, element);
-                }
-                else if (!tmp.has(element.SkillName)) {
-                    tmp.set(element.SkillName, element);
-                }
+        conn.execute("SELECT a.* FROM `16-CardSupportSkills` AS a, `3-IdolCards` AS b WHERE b.CardUUID = ? AND a.CardIndex = b.CardIndex",
+            [CardUUID],
+            (err, result) => {
+                let tmp0 = new Map(),
+                    tmp1 = new Map(),
+                    tmp2 = new Map(),
+                    tmp3 = new Map(),
+                    tmp4 = new Map();
+                result.forEach(element => {
+                    if (element.GainedAt <= 60) {
+                        setMap(element, tmp0);
+                    }
+                    if (element.GainedAt <= 65) {
+                        setMap(element, tmp1);
+                    }
+                    if (element.GainedAt <= 70) {
+                        setMap(element, tmp2);
+                    }
+                    if (element.GainedAt <= 75) {
+                        setMap(element, tmp3);
+                    }
+                    if (element.GainedAt <= 80) {
+                        setMap(element, tmp4);
+                    }
+                });
+                const sm0 = Array.from(tmp0, ([key, value]) => value),
+                    sm1 = Array.from(tmp1, ([key, value]) => value),
+                    sm2 = Array.from(tmp2, ([key, value]) => value),
+                    sm3 = Array.from(tmp3, ([key, value]) => value),
+                    sm4 = Array.from(tmp4, ([key, value]) => value);
+                res([sm0, sm1, sm2, sm3, sm4]);
             });
-            const ls = Array.from(tmp, ([key, value]) => value);
-            res(ls);
-        })
     });
+}
+
+function setMap(e, map) {
+    if ((map.has(e.SkillName) && map.get(e.SkillName).SkillLevel < e.SkillLevel)
+        || !map.has(e.SkillName)) {
+        map.set(e.SkillName, e);
+    }
 }
 
 function DBGetSupportEvents(CardUUID) {
     return new Promise((res, _) => {
         conn.execute("SELECT a.* FROM `19-CardSupportEvents` a, `3-IdolCards` b WHERE a.CardIndex = b.CardIndex AND b.CardUUID = ? ORDER BY `EventID`",
-        [CardUUID], 
-        (err, result) => {
-            res(result);
-        });
-    }); 
+            [CardUUID],
+            (err, result) => {
+                res(result);
+            });
+    });
 }
 
 function DBGetProficiency(CardUUID) {
     return new Promise((res, _) => {
         conn.execute("SELECT a.* FROM `18-CardProficiencies` a, `3-IdolCards` b WHERE a.CardIndex = b.CardIndex AND b.CardUUID = ?",
-        [CardUUID], 
-        (err, result) => {
-            res(result);
-        });
-    }); 
+            [CardUUID],
+            (err, result) => {
+                res(result);
+            });
+    });
 }
 
 function DBGetIdeaMark(CardUUID) {
     return new Promise((res, _) => {
         conn.execute("SELECT a.* FROM `17-CardIdeaMark` a, `3-IdolCards` b WHERE a.CardIndex = b.CardIndex AND b.CardUUID = ?",
-        [CardUUID], 
-        (err, result) => {
-            res(result[0]);
-        });
-    }); 
+            [CardUUID],
+            (err, result) => {
+                res(result[0]);
+            });
+    });
 }
 //14618ede-7ed7-41c9-adec-1b60d1830582
